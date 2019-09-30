e2933df2750f6238f3411013e05765ae492a6329
mohitgupta004 <guptamohi.009@gmail.com>
2019-09-30 18:50:51 +0530

    Added New Event Link

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/contact-us', generalController.contact_us);
 
 router.post('/contact-us', generalController.contact_us_post);
 
-router.get('/cgc-cloud', generalController.cgc_cloud);
+router.get('/actions-on-google', generalController.cgc_cloud);
 
 router.get('/dsc-team', generalController.dsc_full_team);
 
