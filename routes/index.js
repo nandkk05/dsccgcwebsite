85b2ad459e449272d791324f168c257a2b54596c
mohitgupta004 <guptamohi.009@gmail.com>
2019-10-02 21:57:49 +0530

    Added 404

--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,12 @@ router.get('/dsc-team', generalController.dsc_full_team);
 router.get('/gcpcrashcourse', generalController.gcpcrashcourse);
 
 router.get('/gcpcrashcourse-2', generalController.gcpcrashcourse_2);
+router.get('*', function(req, res){
+    res.render('404', {
+        title: 'Cloud Workshop Registration',
+        req
+      })
+  });
+
 
 module.exports = router;
